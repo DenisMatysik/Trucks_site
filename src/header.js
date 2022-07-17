6b980626ef4dbe3adf03838472cfe97ad4432d8b
denis <denismatysik@gmail.com>
2022-07-18 00:48:10 +0300

    some fix

--- src/header.js
+++ src/header.js
@@ -1,13 +1,22 @@
 import data from "./dataset.json";
+import logoPict from "./assets/logo.png";
 
 const rednerHeader = (header) => {
   const headerWrapper = document.querySelector(".header");
-
+  const textWrapper = document.createElement(`div`);
+  textWrapper.classList.add("textWrapper");
   const text = document.createElement(`h1`);
   const title = document.createElement(`div`);
+  const logo = document.createElement("img");
+
   text.innerHTML = `${header.h1}`;
   title.innerHTML = `${header.title}`;
-  headerWrapper.insertAdjacentElement("beforeEnd", text);
+  logo.id = "logo";
+  logo.src = logoPict;
+
+  textWrapper.appendChild(logo);
+  textWrapper.appendChild(text);
+  headerWrapper.insertAdjacentElement("beforeEnd", textWrapper);
   headerWrapper.insertAdjacentElement("beforeEnd", title);
 };
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import data from "./dataset.json";
 import "./sass/main.scss";
 import "./header";
 import "./navigate";
@@ -8,3 +9,19 @@ import "./filterSort";
 import "./pageText";
 import "./images";
 import "./filterParams";
+import logoPict from "./assets/logo.png";
+
+const wrapper = document.createElement("div");
+wrapper.classList.add("titleWrapper");
+const contactWrapper = document.querySelector(".contacts");
+const contactTitle = document.createElement("h2");
+
+const logo = document.createElement("img");
+logo.id = "logo";
+logo.src = logoPict;
+
+wrapper.appendChild(logo);
+wrapper.appendChild(contactTitle);
+
+contactTitle.innerHTML = `${data.page_meta.h1}`;
+contactWrapper.insertAdjacentElement("afterbegin", wrapper);
